00460e600e56de9dd567ff202f393482b9557b18
Vanina Correa <vanina.correa@usach.cl>
2022-09-12 16:11:26 -0300

    [feat] creación test para métodos de OficinaRRHHService

--- src/test/java/tingeso/mueblesstgo/MueblesstgoApplicationTests.java
+++ src/test/java/tingeso/mueblesstgo/MueblesstgoApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import tingeso.mueblesstgo.entities.EmpleadoEntity;
 import tingeso.mueblesstgo.services.EmpleadoService;
+import tingeso.mueblesstgo.services.OficinaRRHHService;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -65,3 +66,60 @@ class EmpleadoTest {
 	 */
 	}
 }
+
+@SpringBootTest
+class OficinaRRHHTest{
+
+	@Autowired
+	private OficinaRRHHService oficinaRRHHService;
+	EmpleadoEntity empleado = new EmpleadoEntity();
+	OficinaRRHHService oficinaRRHH = new OficinaRRHHService();
+
+	// Calcula el sueldo fijo según categoría del empleado
+	@Test
+	void testAsignarSueldoFijo() {
+		empleado.setCategoria("A");
+		oficinaRRHH.asignarSueldoFijo(empleado);
+		assertEquals(1700000, empleado.getSueldoFijo());
+	}
+
+	// Calcula los años de servicio del empleado
+	@Test
+	void testCalcularAniosServicio() {
+		// Asigna fecha de ingreso
+		Calendar fechaIngreso = Calendar.getInstance();
+		fechaIngreso.set(2014,05,05);
+		empleado.setFechaIngreso(fechaIngreso);
+		// Calcula años de servicio, considerando 2022 como el año actual, deberían ser 8 años
+		oficinaRRHH.calcularAniosServicio(empleado);
+		assertEquals(8, empleado.getAniosServicio());
+	}
+
+	@Test
+	void testCalcularBono() {
+		empleado.setCategoria("A");
+		oficinaRRHH.asignarSueldoFijo(empleado);
+		Calendar fechaIngreso = Calendar.getInstance();
+		fechaIngreso.set(2014,05,05);
+		empleado.setFechaIngreso(fechaIngreso);
+		oficinaRRHH.calcularAniosServicio(empleado);
+		double bono = oficinaRRHH.calcularBonoAniosServicio(empleado);
+		assertEquals(85000, bono);
+	}
+
+	@Test
+	void testMontoHorasExtras() {
+		empleado.setCategoria("A");
+		double montoHorasExtras = oficinaRRHH.montoHorasExtras(empleado);
+		assertEquals(25000, montoHorasExtras);
+	}
+
+	@Test
+	void testDescuentoMinutos() {
+		empleado.setCategoria("A");
+		oficinaRRHH.asignarSueldoFijo(empleado);
+		double descuentoMinutos = oficinaRRHH.montoDescuentoAtrasos(empleado, 20);
+		assertEquals(17000, descuentoMinutos);
+	}
+}
+
