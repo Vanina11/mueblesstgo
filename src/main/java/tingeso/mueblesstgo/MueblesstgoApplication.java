ba1b8f447333355986703fb18f5c412dc623af77
Vanina Correa <vanina.correa@usach.cl>
2022-09-12 15:54:35 -0300

    [build] instalación proyecto

---
+++ src/main/java/tingeso/mueblesstgo/MueblesstgoApplication.java
@@ -0,0 +1,13 @@
+package tingeso.mueblesstgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MueblesstgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MueblesstgoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tingeso/mueblesstgo/MueblesstgoApplicationTests.java
@@ -0,0 +1,13 @@
+package tingeso.mueblesstgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MueblesstgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
